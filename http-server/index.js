72936510677e38f8282e7f8c5b4cde7aa747f64c
atiyakhan15 <100838097+atiyakhan15@users.noreply.github.com>
2023-07-13 17:33:23 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -3,3 +3,47 @@ const hello = () => {
 };
 
 hello();
+const http_var = require("http");
+const fs_var = require("fs");
+
+let Content_home = "";
+let Content_project = "";
+
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+
+fs_var.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+
+  fs_var.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  Content_project = project;
+});
+
+  
+  http_var
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(Content_project);
+        response.end();
+        break;
+      default:
+        response.write(Content_home);
+        response.end();
+        break;
+    }
+  })
+  .listen(3000);
