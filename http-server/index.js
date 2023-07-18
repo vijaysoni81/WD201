d7863668875c2202ada4ddedce5c8f6f89e51ca8
atiyakhan15 <100838097+atiyakhan15@users.noreply.github.com>
2023-07-18 15:58:30 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -8,30 +8,30 @@ const fs_var = require("fs");
 
 let Content_home = "";
 let Content_project = "";
+let Content_registration = "";
 
-
-fs.readFile("home.html", (err, home) => {
+fs_var.readFile("./home.html", (err, home) => {
   if (err) {
     throw err;
   }
-  homeContent = home;
+ Content_home = home;
 });
 
-
-fs_var.readFile("home.html", (err, home) => {
-  if (err) {
-    throw err;
-  }
-
-  fs_var.readFile("project.html", (err, project) => {
+fs_var.readFile("./project.html", (err, project) => {
   if (err) {
     throw err;
   }
   Content_project = project;
 });
 
+fs_var.readFile("./registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  Content_registration = registration;
+});
   
-  http_var
+ http
   .createServer((request, response) => {
     let url = request.url;
     response.writeHeader(200, { "Content-Type": "text/html" });
@@ -40,10 +40,13 @@ fs_var.readFile("home.html", (err, home) => {
         response.write(Content_project);
         response.end();
         break;
+      case "/registration":
+        response.write(Content_registration);
+        response.end();
+        break;
       default:
         response.write(Content_home);
         response.end();
         break;
     }
-  })
-  .listen(3000);
+  }).listen(args.port);
